4a268baf06a30f34b7f76c853b59f834914bedb0
bora <34098087+larnon@users.noreply.github.com>
2018-01-19 12:16:38 +0300

    Delete choose_friend.php

--- cotzProject/cotz/choose_friend.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['usrNameOwn']) and !isset($_POST['talk'])){
-  die( header( 'location: /index.html' ) );
-}
-$_SESSION['friendToTalk'] = $_POST['friendToTalk'];
-header( 'location: /talk.php' );
-?>
